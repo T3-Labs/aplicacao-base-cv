2d1dc6d4242407d02d881857eaf962adb6ab8da9
Andre Emidio <andresjc2008@gmail.com>
2023-01-28 21:27:12 -0300

    Refactoring project base

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,7 +1,8 @@
-from models.manager import Manager
-from views.view import MainWindow
-from models.resultshandler import ResultsHandler
 from PyQt5.QtCore import QObject
+
+from models.manager import Manager
+from models.resultshandler import ResultsHandler
+from views.view import MainWindow
 
 
 class MainController(QObject):
@@ -9,19 +10,10 @@ class MainController(QObject):
     def __init__(self) -> None:
         super().__init__()
         self.manager = Manager()
-        #self.resultshandler = ResultsHandler()
         self.window = MainWindow()
         self.manager.postprocess.sendFrame.connect(self.window.show_frame)
         self.manager.postprocess.sendCount.connect(self.window.refresh_text)
 
     def run(self) -> None:
-        #self.resultshandler.sendFrame.connect(self.window.show_frame)
-        #self.resultshandler.sendValuesToView.connect(self.window.refresh_text)
-        
-
-
         self.window.showMaximized()
-        #self.resultshandler.start()
         self.manager.start()
-
-
