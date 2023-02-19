d885876071cce48680ca6f3ca7585f05774eae95
Andre Emidio <andresjc2008@gmail.com>
2023-02-19 12:15:40 -0300

    Add __init__ file to call classes and methods

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+from .camera import Camera
+from .detector import Detector
+from .drawner import Drawner
+from .kalman import KalmanFilter
+from .manager import Manager
+from .postprocess import PostProcess
+from .qmqt import QMQT
+from .resultshandler import ResultsHandler
+from .tracker import Track, Tracker
